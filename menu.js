d0d0b062ac85044c2f047bac2d29d688a195b85d
JSegovia <acosta.segoviaa@hotmail.com>
2022-03-31 10:18:02 -0500

    menú

--- menu.js
+++ menu.js
@@ -1,3 +1,21 @@
 document.querySelector('#menu-icon').addEventListener('click', () => {
   document.querySelector('.menu-toggle').classList.toggle('sidebar-menu');
 });
+document.querySelector('#inicio-item').addEventListener('click', () => {
+  document.querySelector('.menu-toggle').classList.toggle('sidebar-menu');
+});
+document.querySelector('#essenza-item').addEventListener('click', () => {
+  document.querySelector('.menu-toggle').classList.toggle('sidebar-menu');
+});
+document.querySelector('#amenidades-item').addEventListener('click', () => {
+  document.querySelector('.menu-toggle').classList.toggle('sidebar-menu');
+});
+document.querySelector('#master-plan-item').addEventListener('click', () => {
+  document.querySelector('.menu-toggle').classList.toggle('sidebar-menu');
+});
+document.querySelector('#contacto-item').addEventListener('click', () => {
+  document.querySelector('.menu-toggle').classList.toggle('sidebar-menu');
+});
+document.querySelector('#ubicacion-item').addEventListener('click', () => {
+  document.querySelector('.menu-toggle').classList.toggle('sidebar-menu');
+});
